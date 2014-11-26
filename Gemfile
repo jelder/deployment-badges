4e86a821f4fd53b6769f3d479650fefb5ffe9894
Jacob Elder <jacob@boundless.com>
2014-11-25 20:15:22 -0500

    Kinda works

--- Gemfile
+++ Gemfile
@@ -5,8 +5,13 @@ gem "sinatra-contrib"
 # gem "sinatra-partial", require: 'sinatra/partial'
 gem "haml"
 gem "newrelic_rpm"
-gem "passenger", "5.0.0.beta1"
+gem "passenger"
 gem "hiredis"
 gem "redis"
 gem "awesome_print"
-gem "pry"
+
+# group "development" do
+  gem "pry"
+  gem "binding_of_caller"
+  gem "better_errors"
+# end
--- app.rb
+++ app.rb
@@ -8,11 +8,52 @@ class DeploymentBadges < Sinatra::Base
 
   set :haml, { :ugly => production?, :format => :html5 }
   set :public_folder, File.dirname(__FILE__) + '/static'
+  
+  configure :development do
+    use BetterErrors::Middleware
+    BetterErrors.application_root = __dir__
 
-  post '/' do
-
+    register Sinatra::Reloader
   end
 
+  post '/badges/:id' do
+    if params[:secret_key] != secret_key
+      status 403 and return
+    end
+    $redis.hmset params[:id],
+      :github, params[:github],
+      :commit_hash, params[:commit_hash],
+      :updated_at, Time.now.to_i
+  end
 
+  get '/badges/:id.svg' do
+    if app = fetch_app(params[:id])
+      content_type "image/svg+xml"
+      erb :badge, locals: app
+    else
+      status 404 and return
+    end
+  end
+
+  get '/badges/:id' do
+    if app = fetch_app(params[:id])
+      body app.inspect
+      redirect "https://github.com/#{app[:github]}/tree/#{app[:commit_hash]}"
+    else
+      status 404 and return
+    end
+  end
+
+  private
+
+    def secret_key
+      ENV.fetch("SECRET_KEY")
+    end
+
+    def fetch_app(id)
+      result = $redis.hmget params[:id], :github, :commit_hash, :updated_at
+      return if result.compact.empty?
+      [:github, :commit_hash, :updated_at].zip(result).to_h
+    end
 
 end
