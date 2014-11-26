f2de4d044722bf3d757750a323129c6380a1011c
Jacob Elder <jacob@boundless.com>
2014-11-26 16:26:27 -0500

    Typo

--- app.rb
+++ app.rb
@@ -54,7 +54,7 @@ class DeploymentBadges < Sinatra::Base
 
   get '/badges/:id/github' do
     if resource = Resource.find(params[:id])
-      redirect "https://github.com/#{resource[:github]}/tree/#{app[:commit_hash]}"
+      redirect "https://github.com/#{resource[:github]}/tree/#{resource[:commit_hash]}"
     else
       body "Not found"
       status 404
