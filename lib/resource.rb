12fe74acd1c1f5d3f994f53c5b646a389a57faa2
Jacob Elder <jacob@boundless.com>
2014-12-01 13:44:26 -0500

    Rework SVG to not use any javascript

--- lib/resource.rb
+++ lib/resource.rb
@@ -40,6 +40,26 @@ class Resource
     "https://github.com/#{@attributes[:github]}/compare/master...#{@attributes[:head_long]}"
   end
 
+  def age
+    (Time.now.to_i - attributes[:updated_at].to_i)
+  end
+
+  def color
+    case age / (60 * 60)
+    when  0...2  then "#7ac631"
+    when  2...4  then "#ffda39"
+    when  4...8  then "#f9b600"
+    when  8...16 then "#f4932c"
+    when 16...32 then "#ff6000"
+    else
+      "#e53f00"
+    end
+  end
+
+  def style
+    "fill: #{color}"
+  end
+
   def save
     @attributes.symbolize_keys!
     @attributes.assert_valid_keys(FIELDS)
